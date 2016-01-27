97489121fa0281abedb2abfc1a1f7c700860364a
nepochal <development@mischolz.de>
2016-01-27 09:04:41 +0100

    Added licence headers to all cs-files

--- redshift-tray/redshift.cs
+++ redshift-tray/redshift.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿/* This file is part of redshift-tray.
+   Redshift-tray is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   Redshift-tray is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with redshift-tray.  If not, see <http://www.gnu.org/licenses/>.
+   Copyright (c) Michael Scholz <development@mischolz.de>
+*/
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;

2ab4bd8843668cc915f8f40af3f87d3bf1ec1184
nepochal <development@mischolz.de>
2016-02-16 13:12:25 +0100

    Added process starts to all links in the about dialog

--- redshift-tray/About.xaml.cs
+++ redshift-tray/About.xaml.cs
@@ -1,5 +1,19 @@
-﻿using System;
+﻿/* This file is part of redshift-tray.
+   Redshift-tray is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   Redshift-tray is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with redshift-tray.  If not, see <http://www.gnu.org/licenses/>.
+   Copyright (c) Michael Scholz <development@mischolz.de>
+*/
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +36,11 @@ namespace redshift_tray
     public About()
     {
       InitializeComponent();
+    }
+
+    private void RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+    {
+      Process.Start(e.Uri.ToString());
     }
   }
 }

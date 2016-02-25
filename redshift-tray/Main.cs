2fee71aecc73e217096b594d85f6f542ed9001e5
nepochal <development@mischolz.de>
2016-02-25 13:39:01 +0100

    Increased version no.

--- redshift-tray/Main.cs
+++ redshift-tray/Main.cs
@@ -20,7 +20,7 @@ namespace redshift_tray
   class Main
   {
 
-    public const string VERSION = "0.4.1a";
+    public const string VERSION = "0.4.2a";
 
     private static DebugConsole debugConsole;
 

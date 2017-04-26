a5a58aedcb6aef072dd76092355c1462788396bf
nepochal <development@mischolz.de>
2017-04-26 09:35:19 +0200

    Fixed format

--- redshift-tray.Test/CommonTests.cs
+++ redshift-tray.Test/CommonTests.cs
@@ -3,15 +3,15 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace redshift_tray.Test
 {
-    [TestClass]
-    public class CommonTests
+  [TestClass]
+  public class CommonTests
+  {
+    [TestMethod]
+    public void LocationCanBeParsed()
     {
-        [TestMethod]
-        public void LocationCanBeParsed()
-        {
-            AutoLocation autoLocation = Common.ParseLocation("50.8476", "4.3428");
-            Assert.AreEqual(50.8476M, autoLocation.Latitude);
-            Assert.AreEqual(4.3428M, autoLocation.Longitude);
-        }
+      AutoLocation autoLocation = Common.ParseLocation("50.8476", "4.3428");
+      Assert.AreEqual(50.8476M, autoLocation.Latitude);
+      Assert.AreEqual(4.3428M, autoLocation.Longitude);
     }
+  }
 }

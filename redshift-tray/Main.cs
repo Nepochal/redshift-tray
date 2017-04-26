ed74fb9725e4eb3a60bc9a31ae4949774a5131e4
nepochal <development@mischolz.de>
2017-04-26 10:09:48 +0200

    Increased version No.

--- redshift-tray/Main.cs
+++ redshift-tray/Main.cs
@@ -10,7 +10,7 @@ namespace redshift_tray
   class Main
   {
 
-    public const string VERSION = "0.6.1-alpha";
+    public const string VERSION = "0.7.0-beta";
 
     public const string RELEASES_PAGE = "https://github.com/jonls/redshift/releases";
 
